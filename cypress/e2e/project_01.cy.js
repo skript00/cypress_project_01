8fa204866b87af522b8ab469563401fcc36fef75
ameers00 <am33rsaleh6@gmail.com>
2024-05-14 04:00:38 -0500

    added cypress cheatsheet & finished hw

--- cypress/e2e/project_01.cy.js
+++ cypress/e2e/project_01.cy.js
@@ -13,9 +13,9 @@ describe("Homework01", () => {
   });
 
   it("Test Case 02 - Validate the Full name input box", () => {
-    cy.get('.field > label').eq(0).should("have.text", "Full name *")
-    cy.get('.control > .input').eq(0).should('be.visible')
-    .and('have.attr', 'required')
+    cy.get('.field > .label').eq(0).should("have.text", "Full name *")
+
+    cy.get('.control > .input').eq(0).should('be.visible').and('have.attr', 'required')
     cy.get('.control > .input').eq(0).should("have.attr",'placeholder', "Enter your full name") 
   });
 
@@ -32,15 +32,14 @@ describe("Homework01", () => {
 
     cy.get(".radio input").eq(0).click();
 
-    cy.get(".radio").each(($el, index) => {
+    cy.get(".radio").each(($el) => {
       if ($el.text() === "Male") cy.wrap($el).children().should("be.checked");
       else cy.wrap($el).children().should("not.be.checked");
     });
 
     cy.get(".radio input").eq(1).click();
 
-
-    cy.get('.radio').each(($el, index) => {
+    cy.get('.radio').each(($el) => {
       if ($el.text() === 'Female') cy.wrap($el).children().should('be.checked')
       else cy.wrap($el).children().should("not.be.checked");
     })
@@ -48,40 +47,63 @@ describe("Homework01", () => {
   });
 
   it("Test Case 04 - Validate the Address input box", () => {
-    cy.get('.field .label').eq(1).should('be.visible')
-    .and('not.have.attr', 'required')
-
+    cy.get('.label').eq(1).should('be.visible').and('not.have.attr', 'required')
     cy.get('.label').eq(2).should('have.text', 'Address')
+
     cy.get('.input').eq(1).should('have.attr', 'placeholder', 'Enter your address')
   });
 
-  it.only("Test Case 05 - Validate the Email input box", () => {
-    cy.get('.field .input').eq(2).should('be.visible')
-    .and('have.attr', 'required')
-
-    cy.get('.label').eq(3).should('have.text', 'Email *')
+  it("Test Case 05 - Validate the Email input box", () => {
+    cy.get('.input').eq(2).should('be.visible').and('have.attr', 'required')
     cy.get('.input').eq(2).should('have.attr', 'placeholder', 'Enter your email')
 
+    cy.get('.label').eq(3).should('have.text', 'Email *')
   });
 
   it("Test Case 06 - Validate the Phone input box", () => {
-    
+    cy.get('.input').eq(3).should('be.visible').and('not.have.attr', 'required')
+    cy.get('.input').eq(3).should('have.attr', 'placeholder', 'Enter your phone number')
+
+    cy.get('.label').eq(4).should('have.text', 'Phone')
   });
 
   it("Test Case 07 - Validate the Message text area", () => {
-    
+    cy.get('.textarea').should('be.visible').and('not.have.attr', 'required')
+    cy.get('.textarea').should('have.attr', 'placeholder', 'Type your message here...')
+
+    cy.get('.label').eq(5).should('have.text', 'Message')
   });
 
   it("Test Case 08 - Validate the Consent checkbox", () => {
-    
+    cy.get('.checkbox').should('have.text', ' I give my consent to be contacted.')
+
+    cy.get('.checkbox > input').should('have.attr', 'required')
+    cy.get('.checkbox > input').should('be.enabled')
+      .check().and('be.checked').uncheck().and('not.be.checked')
   });
 
   it("Test Case 09 - Validate the SUBMIT button", () => {
-    
+    cy.get('.control .button').should('be.visible')
+      .and('be.enabled').and('contain', 'SUBMIT')
+
   });
 
   it("Test Case 10 - Validate the form submission", () => {
+    cy.get('.control > .input').eq(0).type('SKRIPT')
+    cy.get(".radio input").eq(0).click();
+    cy.get('.input').eq(1).type('1234 Skript Ave, Skriptville IL, 90210')
+    cy.get('.input').eq(2).type('Krisco_Skript_Kratos@email.com')
+    cy.get('.input').eq(3).type('773-202-5862')
+    cy.get('.textarea').type('THIS WILL WORK, God Willing!')
+    cy.get('.checkbox > input').check()
+    cy.get('.control > .button').click()
+    cy.get('.mt-5').should('have.text', 'Thanks for submitting!')
+
+    cy.on('uncaught:exception', () => {
+      return false
+    })
     
   });
 
 });
+
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,88 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+
+/*
+
+cy.visit(‘foobar.com’)
+cy.reload()
+cy.go(‘back’) or cy.go(-1) 
+cy.go(‘forward’) or cy.go(1)
+cy.title().should(‘eq’, ’Techglobal Training | Home’)
+cy.url().should(‘eq’, ‘foobar.com’)
+expect(true).to.equal(true)
+cy.get(#logo)
+.click()
+.should('be.visible')
+
+div > a (immediate child)
+div a (any grandchild)
+a + a (immediate adjacent sibling at same hierarchy)
+p, span (groups multiple selectors)
+
+:nth-child(1)
+:not()
+:checked
+:disabled or :enabled
+:empty
+:required and :optional
+
+cy.get('#user_email').eq(1)
+cy.get('.locator')contains('element', 'content')
+cy.get('parentElement').find('childElement')
+
+cy.next() or cy.prev() (traverse immediate sibling)
+cy.get('childElement').parent('parentElement') (parent must be chained)
+
+.click()
+.type()
+.clear()
+.check()
+.uncheck()
+.select() (select value in dropdown)
+.trigger() (manually trigger DOM event)
+
+//implicit assertion
+.should('have.attr', 'placeholder', 'random placeholder')
+.and('have.class', 'active')
+
+//explicit assertion
+cy.get('#element').invoke('text').then((text) => {
+  expect(text).to.equal('Text to match')
+})
+
+cy.get('#element').invoke('attr', 'attribute-name').then((attributeValue) => {
+  expect(attributeValue).to.equal('attribute-value')
+})
+
+cy.get('[for="email"]').then(($txt) => {
+  const actualText = $txt.text()   
+      //this is using jQuery's .text() method to extract text content of element
+      //now it's an actual text value
+      //and we can use JS methods on it
+  console.log(actualText.trim())
+  console.log(actualText.length)
+})
+
+
+cy.get('.list-items').each(($item, index) => {
+  const itemText = $item.text()
+  console.log(`${index}: ${itemText.trim()}`)
+    //this creates a loop to extract .each() item using jQuery method
+    //similar to .then()
+})
+
+cy.get('#element').then(($el) => {
+  cy.wrap($el).should('be.visible').click()
+    //^^wrapping DOM element to chain w/ cypress commands
+})
+
+cy.get('element').invoke('text').then((text) => {
+    //here, .invoke() extracts text content from web element
+    //now it's a plain JS string and not chainable
+    //because it became a regular JS object
+    //in order to perform cypress commands, wrap it
+  cy.wrap(text).should('equal', 'Expected Text')
+})
+
+*/
